96be3acda9be036b2fb930553f35323771e00c20
Mahendra_Choudhary <mahendrajat072003@gmail.com>
2024-01-27 19:52:12 +0530

    login signup form

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 import { BrowserRouter, Link } from "react-router-dom";
 import { Routes, Route } from "react-router";
 import Home from "./Pages/Home/Home.jsx";
-// import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';;
 import PostCard from "./Components/PostCard/PostCard.jsx";
+import LoginSignUp from "./Components/LoginSignup/LoginSignup.jsx";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/postCard" element={<PostCard />} />
+          <Route path="auth" element={<LoginSignUp />} />
         </Routes>
       </BrowserRouter>
     </div>
