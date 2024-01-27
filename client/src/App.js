d3b6b5f5323380f300861493688e56aa48a98b96
Mahendra_Choudhary <mahendrajat072003@gmail.com>
2024-01-27 21:48:33 +0530

    store

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router";
 import Home from "./Pages/Home/Home.jsx";
 import PostCard from "./Components/PostCard/PostCard.jsx";
 import LoginSignUp from "./Components/LoginSignup/LoginSignup.jsx";
+import Post from "./Pages/Post/PostPage.jsx";
+import Store from "./Pages/Store/Store.jsx";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/postCard" element={<PostCard />} />
           <Route path="auth" element={<LoginSignUp />} />
+          <Route path="post" element={<Post />} />
+          <Route path="store" element={<Store />} />
         </Routes>
       </BrowserRouter>
     </div>
