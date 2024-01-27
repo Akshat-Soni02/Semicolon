888f194c1da3e14fd3a53d5721456ae4279248f3
dibyam-jalan27 <dibyam1jalan@gmail.com>
2024-01-28 00:31:27 +0530

    gfdsdfghkjhfdfgui

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,29 @@ import PostCard from "./Components/PostCard/PostCard.jsx";
 import LoginSignUp from "./Components/LoginSignup/LoginSignup.jsx";
 import Post from "./Pages/Post/PostPage.jsx";
 import Store from "./Pages/Store/Store.jsx";
-import contPo from "./Pages/contentPolicy/contPo.jsx"
-import Mod from "./Pages/ModeratorCodeOfConduct/Mod.jsx"
-import PrivPol from "./Pages/privacyPolicy/PrivPol.jsx"
-import UserAgg from "./Pages/UserAgreement/UserAgg.jsx"
+import contPo from "./Pages/contentPolicy/contPo.jsx";
+import Mod from "./Pages/ModeratorCodeOfConduct/Mod.jsx";
+import PrivPol from "./Pages/privacyPolicy/PrivPol.jsx";
+import UserAgg from "./Pages/UserAgreement/UserAgg.jsx";
+import CommunityPage from "./Pages/Community/CommunityPage.jsx";
+import Navbar from "./Components/Navbar/Navbar.jsx";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/postCard" element={<PostCard />} />
-          <Route path="auth" element={<LoginSignUp />} />
+          <Route path="/login" element={<LoginSignUp />} />
           <Route path="post" element={<Post />} />
           <Route path="store" element={<Store />} />
-          <Route path='/stars/codeOfConduct' element = {<Mod/>} />
-          <Route path='/ContentPolicy' element = {<contPo/>} />
-          <Route path='/PrivacyPolicy' element = {<PrivPol/>} />
-          <Route path='/UserAgreement' element = {<UserAgg/>} />
+          <Route path="/stars/codeOfConduct" element={<Mod />} />
+          <Route path="/ContentPolicy" element={<contPo />} />
+          <Route path="/PrivacyPolicy" element={<PrivPol />} />
+          <Route path="/UserAgreement" element={<UserAgg />} />
+          <Route path="/community" element={<CommunityPage />} />
         </Routes>
       </BrowserRouter>
     </div>
